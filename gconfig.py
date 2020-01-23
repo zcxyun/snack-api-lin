c6b6d7fb7e7ab6f4b0402e59f49eca643f66ace4
zcxyun <zcxyun@126.com>
2020-01-23 16:00:12 +0800

    update .gitignore gconfig.py

--- gconfig.py
+++ gconfig.py
@@ -1,10 +1,13 @@
 from gevent import monkey
 monkey.patch_all()
 import multiprocessing
-debug = True
-loglevel = 'debug'
+debug = False
+reload = True
+loglevel = 'info'
 bind = 'localhost:5000'  # 绑定与Nginx通信的端口
 pidfile = 'glogs/gunicorn.pid'
-logfile = 'glogs/debug.log'
-workers = multiprocessing.cpu_count()
+errorlog = 'glogs/error.log'
+accesslog = 'glogs/access.log'
+workers = multiprocessing.cpu_count() * 2 + 1
+#workers = 1
 worker_class = 'gevent' # 默认为阻塞模式，最好选择gevent模式
